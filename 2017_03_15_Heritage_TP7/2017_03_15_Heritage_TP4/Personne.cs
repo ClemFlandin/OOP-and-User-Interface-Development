d8186fb0697428621ef88a34b0fb0638434f4387
Clément Flandin <clemfla@live.fr>
2017-03-20 09:20:27 +0100

    new commit

---
+++ 2017_03_15_Heritage_TP7/2017_03_15_Heritage_TP4/Personne.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_03_15_Heritage_TP4
+{
+    public abstract class Personne
+    {
+        private string nom;
+        public string Nom
+        {
+            get
+            {
+                return this.nom;
+            }
+            set
+            {
+                this.nom = value;
+            }
+        }
+
+    }
+}
