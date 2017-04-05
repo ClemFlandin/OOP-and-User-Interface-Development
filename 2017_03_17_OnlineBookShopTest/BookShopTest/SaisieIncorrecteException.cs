d1fb6ec9c5a27406531c3eaa903d851d94b83484
Clément Flandin <clemfla@live.fr>
2017-04-05 13:37:36 +0200

    ajout deux exceptions

---
+++ 2017_03_17_OnlineBookShopTest/BookShopTest/SaisieIncorrecteException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShopTest
+{
+    public class SaisieIncorrecteException : Exception
+    {
+        public SaisieIncorrecteException()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return "Votre saisie est incorrecte, veuillez effectuer une saisie correcte.";
+        }
+
+    }
+}
+
